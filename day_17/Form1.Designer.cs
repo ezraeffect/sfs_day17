717b13e0455ce1117fb504005901da4cc445f0ed
ezraeffect <build@fx0.kr>
2025-05-16 09:51:50 +0900

    초기 커밋.

---
+++ day_17/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace day_17
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_ID = new System.Windows.Forms.TextBox();
+            this.textBox_PW = new System.Windows.Forms.TextBox();
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox_ID
+            // 
+            this.textBox_ID.Location = new System.Drawing.Point(12, 12);
+            this.textBox_ID.Name = "textBox_ID";
+            this.textBox_ID.Size = new System.Drawing.Size(204, 21);
+            this.textBox_ID.TabIndex = 0;
+            // 
+            // textBox_PW
+            // 
+            this.textBox_PW.Location = new System.Drawing.Point(12, 39);
+            this.textBox_PW.Name = "textBox_PW";
+            this.textBox_PW.PasswordChar = '*';
+            this.textBox_PW.Size = new System.Drawing.Size(204, 21);
+            this.textBox_PW.TabIndex = 1;
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.Location = new System.Drawing.Point(222, 12);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(75, 48);
+            this.buttonLogin.TabIndex = 2;
+            this.buttonLogin.Text = "로그인";
+            this.buttonLogin.UseVisualStyleBackColor = true;
+            this.buttonLogin.Click += new System.EventHandler(this.buttonLogin_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(309, 82);
+            this.Controls.Add(this.buttonLogin);
+            this.Controls.Add(this.textBox_PW);
+            this.Controls.Add(this.textBox_ID);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_ID;
+        private System.Windows.Forms.TextBox textBox_PW;
+        private System.Windows.Forms.Button buttonLogin;
+    }
+}
+
---
+++ day_17/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace day_17
+{
+    public partial class Form1 : Form
+    {
+        public Dictionary<string, string> idPw = new Dictionary<string, string>();
+        public Dictionary<string, string> idTel = new Dictionary<string, string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // 1. OpenFileDialog를 활용해 사용자 정보 불러오기
+            var fileContent = string.Empty;
+            var filePath = string.Empty;
+
+            // 2. 2개의 제네릭 딕셔너리를 만들고
+            
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    filePath = openFileDialog.FileName;
+
+                    //Read the contents of the file into a stream
+                    var fileStream = openFileDialog.OpenFile();
+
+                    using (StreamReader reader = new StreamReader(fileStream))
+                    {
+                        //fileContent = reader.ReadToEnd();
+
+                        string line;
+                        while ((line = reader.ReadLine()) != null)
+                        {
+                            string[] splitedLine = line.Split(',');
+                            // 2-1. 아이디, 비밀번호
+                            // 2-2. 아이디, 전화번호 저장. (전화번호가 없다면 null 저장)
+                            if (splitedLine.Length <= 1)
+                            {
+                                MessageBox.Show("입력된 계정 정보가 잘못되었습니다");
+                            } else if (splitedLine.Length == 2)
+                            {
+                                idPw.Add(splitedLine[0], splitedLine[1]);
+                                idTel.Add(splitedLine[0], null);
+                            } else if (splitedLine.Length == 3)
+                            {
+                                idPw.Add(splitedLine[0], splitedLine[1]);
+                                idTel.Add(splitedLine[0], splitedLine[2]);
+                            }
+                        }
+                    }
+                }
+            }
+
+            //MessageBox.Show(fileContent, "File Content at path: " + filePath, MessageBoxButtons.OK);
+
+            // 2-1. 아이디, 비밀번호
+            // 2-2. 아이디, 전화번호 저장. (전화번호가 없다면 null 저장)
+            // 3. 로그인 창을 만들고 로그인 성공/실패 메시지 박스 띄우기
+            // 3-1. 로그인 성공시 메시지박스에 아이디, 전화번호도 띄우기
+            // 3-2. 실패 시 메시지박스에 실패 이유 보여주기
+        }
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            if (idPw.ContainsKey(textBox_ID.Text))
+            {
+                if (textBox_PW.Text == idPw[textBox_ID.Text])
+                {
+                    string telNum = idTel[textBox_ID.Text];
+                    if (telNum == null)
+                    {
+                        MessageBox.Show($"{textBox_ID.Text}님의 전화번호가 없습니다.", "로그인 성공", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        MessageBox.Show($"{textBox_ID.Text}님의 전화번호는 {telNum}입니다.", "로그인 성공", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+
+                }
+                else if (textBox_PW.Text != idPw[textBox_ID.Text])
+                {
+                    MessageBox.Show("비밀번호가 일치하지 않습니다.", "로그인 실패", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("아이디가 존재하지 않습니다.", "로그인 실패", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+
+        }
+    }
+}
